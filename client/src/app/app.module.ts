8b5ae23ff760b0e27e521c1344031ec5dbe54aaa
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:30:12 +0300

    Removed deprecated bootstrap array to accommodate the standalone AppComponent. Added PrimeNG modules (ButtonModule, CardModule, CheckboxModule) to enhance UI features.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,32 @@
-// client/src/app/app.module.ts
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material.module"; // Import Angular Material Module
-
-import { AppComponent } from "./app.component";
-import { ProductCarouselComponent } from "./components/product-carousel/product-carousel.component";
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
-import { OrderHistoryComponent } from "./components/order-history/order-history.component";
-import { PaymentGatewayComponent } from "./components/payment-gateway/payment-gateway.component";
+import { MaterialModule } from "./material.module"; // Angular Material Module
+import { HomeComponent } from "../Components/home/home.component";
+import { LoginComponent } from "../Components/Login&Registration/Login/login.component";
+import { SignupComponent } from "../Components/Login&Registration/Signup/signup.component";
+import { ProductCarouselComponent } from "../Components/product-carousel/product-carousel.components";
+import { ProductListComponent } from "../Components/product-list/product-list.component";
+import { ShoppingCartComponent } from "../Components/shopping-cart/shopping-cart.component";
+import { OrderHistoryComponent } from "../Components/order-history/order-history.component";
+import { PaymentGatewayComponent } from "../Components/payment-gateway/payment-gateway.component";
+import { CardModule } from "primeng/card";
+import { CheckboxModule } from "primeng/checkbox";
+import { ButtonModule } from "primeng/button"; // Ensure ButtonModule is imported
 
 @NgModule({
   declarations: [
-    AppComponent,
+    HomeComponent,
     ProductCarouselComponent,
     ProductListComponent,
     ShoppingCartComponent,
     OrderHistoryComponent,
     PaymentGatewayComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,8 +35,11 @@ import { PaymentGatewayComponent } from "./components/payment-gateway/payment-ga
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
+    CardModule,
+    CheckboxModule,
+    ButtonModule, // Add ButtonModule here
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [HomeComponent], // Ensure that you are bootstrapping a non-standalone component
 })
 export class AppModule {}
