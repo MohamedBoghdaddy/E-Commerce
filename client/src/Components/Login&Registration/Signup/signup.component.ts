91b7008b1341b04d934327ba1974ef86f2fe1ced
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:32:34 +0300

    Added form controls for the PrimeNG checkbox and implemented state management logic for the new checked property to provide a more interactive user signup experience.

---
+++ client/src/Components/Login&Registration/Signup/signup.component.ts
@@ -0,0 +1,15 @@
+// client/src/app/components/signup/signup.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-signup',  // HTML tag used to include this component
+  templateUrl: './signup.component.html',  // Reference to the HTML template file
+  styleUrls: ['./signup.component.css']  // Reference to the CSS file
+})
+export class SignupComponent {
+  // Properties and methods for this component go here
+
+  constructor() {
+    // Initialization logic can go here
+  }
+}
