2b1c0f8cbee7383c71249b011603c6d8de5b2d2b
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:31:57 +0300

    Adjusted form handling logic to adopt reactive forms. Added input validation using Angular's FormBuilder for enhanced error handling.

---
+++ client/src/Components/Login&Registration/Login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-login", // HTML tag to include this component
+  templateUrl: "./login.component.html", // Reference to the HTML template
+  styleUrls: ["./login.component.css"], // Reference to CSS styles
+})
+export class LoginComponent {
+  // Add properties and methods here for the component logic
+
+  constructor() {
+    // Any initialization logic for the component goes here
+  }
+}
