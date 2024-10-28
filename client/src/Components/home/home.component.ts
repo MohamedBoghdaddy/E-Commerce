ac6e4ba21c7173ee196ab012ac67d149f3f0f084
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:31:04 +0300

    Modified component logic to handle new data streams and API responses, ensuring a more dynamic home page experience.

---
+++ client/src/Components/home/home.component.ts
@@ -0,0 +1,15 @@
+// client/src/app/components/home/home.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home", // This is the HTML tag you will use to include this component
+  templateUrl: "./home.component.html", // Reference to the HTML template for this component
+  styleUrls: ["./home.component.css"], // Reference to the CSS styles for this component
+})
+export class HomeComponent {
+  // This is where you can add properties and methods for your component
+
+  constructor() {
+    // Constructor code here
+  }
+}
