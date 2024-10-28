068fd8e0ba2bf7b154698733c6e6f42ec87ba953
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:29:14 +0300

    Improved route definitions and authentication guards to enhance security and ensure only authorized users access protected routes.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
-// client/src/app/app-routing.module.ts
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { ProductListComponent } from "../Components/product-list/product-list.component";
+import { ShoppingCartComponent } from "../Components/shopping-cart/shopping-cart.component";
+import { HomeComponent } from "../Components/home/home.component";
+import { LoginComponent } from "../Components/Login&Registration/Login/login.component";
+import { SignupComponent } from "../Components/Login&Registration/Signup/signup.component";
+import { authGuard } from "./guards/auth.guard";
 
-const routes: Routes = [
-  { path: "", component: ProductListComponent },
+export const appRoutes: Routes = [
   { path: "cart", component: ShoppingCartComponent },
-  // Add more routes as necessary
+  { path: "home", component: HomeComponent, canActivate: [authGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent }, // Use lowercase for the path name
+  { path: "productList", component: ProductListComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)], // Use appRoutes here
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
