7e789d1c645d1a71d1c516d22efd6916d7b6c33a
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-28 11:28:51 +0300

    Refactored the application bootstrap to adopt the new Angular bootstrapApplication function. Removed outdated approaches to align with the standalone component structure.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,30 @@
-// client/src/main.ts
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppModule } from "./app/app.module";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+import { appRoutes } from "./app/app-routing.module";
+import { provideHttpClient } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { importProvidersFrom } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "./app/material.module";
+import { CardModule } from "primeng/card";
+import { CheckboxModule } from "primeng/checkbox";
+import { ButtonModule } from "primeng/button";
+import { CommonModule } from "@angular/common";
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(
+      FormsModule,
+      ReactiveFormsModule,
+      MaterialModule,
+      CardModule,
+      CheckboxModule,
+      ButtonModule,
+      CommonModule
+    ),
+  ],
+}).catch((err) => console.error(err));
